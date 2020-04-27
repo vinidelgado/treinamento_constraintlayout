5cdd0a84afc529dda3ec30c7f05e81dca475dfa0
vdelgado <vinnygarciadelgado@gmail.com>
2020-04-27 17:37:11 -0300

    Initial commit

---
+++ app/src/main/java/br/vini/treinamentoconstraintlayout/ScreenTestActivity.kt
@@ -0,0 +1,17 @@
+package br.vini.treinamentoconstraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class ScreenTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+//        setContentView(R.layout.fashion_main_activity)
+        setContentView(R.layout.game_banner_activity)
+    }
+}
